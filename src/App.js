39eeac3a6567511135e6e1597b16c94e28657c48
unknown <ganeshg1582000@gmail.com>
2023-08-21 15:52:36 +0530

    secondary commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import React from "react";
 export default function App() {
     return (
         <>
-        <div>
-            <h1>
-                ihoutyrxjk
-                hitr6tsrxfgvjhkuytrsd
-            </h1>
-        </div>
+       
+       
         </>
     )
 }
