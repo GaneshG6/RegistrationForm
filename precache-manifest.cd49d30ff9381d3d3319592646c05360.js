f2939a28c40a2b6b15e082e7dbbb555ceb8933d4
unknown <ganeshg1582000@gmail.com>
2023-08-21 17:44:52 +0530

    Updates

--- precache-manifest.cd49d30ff9381d3d3319592646c05360.js
+++ precache-manifest.cd49d30ff9381d3d3319592646c05360.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "1c071c9da93c3bcdd213",
-    "url": "/tmsclone/static/css/main.fa482c1b.chunk.css"
+    "revision": "5c5d3fb8c869a34a0e90",
+    "url": "/tmsclone/static/css/main.1aaee619.chunk.css"
   },
   {
-    "revision": "1c071c9da93c3bcdd213",
-    "url": "/tmsclone/static/js/main.1c071c9d.chunk.js"
+    "revision": "5c5d3fb8c869a34a0e90",
+    "url": "/tmsclone/static/js/main.5c5d3fb8.chunk.js"
   },
   {
     "revision": "444f0bc38a785630c6eb",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/tmsclone/static/js/runtime~main.192b9916.js"
   },
   {
-    "revision": "b4d28ca9e2f98a82f0cdd622a14e4088",
+    "revision": "bfec7c62f66e55d98019206089f373ca",
     "url": "/tmsclone/index.html"
   }
 ];
