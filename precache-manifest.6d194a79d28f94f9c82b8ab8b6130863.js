c6f7907634a0e7d169d6067847831a00d141eb5d
unknown <ganeshg1582000@gmail.com>
2023-08-21 15:54:33 +0530

    Updates

---
+++ precache-manifest.6d194a79d28f94f9c82b8ab8b6130863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8fec9a9e788f1e5b2e6",
+    "url": "/tmsclone/static/css/main.595ffa15.chunk.css"
+  },
+  {
+    "revision": "c8fec9a9e788f1e5b2e6",
+    "url": "/tmsclone/static/js/main.c8fec9a9.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/tmsclone/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "364049ffe82b37a1bd83",
+    "url": "/tmsclone/static/js/2.364049ff.chunk.js"
+  },
+  {
+    "revision": "192b991641e086d68009",
+    "url": "/tmsclone/static/js/runtime~main.192b9916.js"
+  },
+  {
+    "revision": "1afaa4638a450bf25ccbb54a1d80365d",
+    "url": "/tmsclone/index.html"
+  }
+];
