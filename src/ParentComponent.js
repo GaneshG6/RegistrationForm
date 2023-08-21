0240125e6941a07dccb0ed87515271c9dafd5349
unknown <ganeshg1582000@gmail.com>
2023-08-21 17:31:05 +0530

    changes

--- src/ParentComponent.js
+++ src/ParentComponent.js
@@ -8,8 +8,16 @@ const [error, setError] = useState({name: "", age: "", gender: "", mobile: "", e
 const [store, setStore] = useState([])
 const [upload, setUpload] = useState(false)
 const [print, setPrint] = useState(false)
+const [edit, setEdit] = useState(false)
+const [save, setSave] = useState(false)
 console.log(form,6);
 console.log(upload, "up");
+
+// const handleEditClick = (()=>{
+//     setEdit(!edit)
+// })
+
+
     return (
         <>
          <div className='container'>
@@ -25,10 +33,12 @@ console.log(upload, "up");
            />
         </div>
        
-       { print && <div className='details-container'>
+       {/* { print && <div className='details-container'>
             {store.map((details)=>{
-                return <div className='details-container2' >
+                return edit == false ? <div className='details-container2' >
                  <h1 className='heading'>User Details Of {details.name}</h1>
+                 <button onClick={handleEditClick}>Edit</button>
+                 <button onClick={handleEditClick}>Save</button>
                  <div className='details'>
                  <li> <b>Name :</b> {details.name}</li> 
                   <li> <b>Age : </b>{details.age}</li>
@@ -37,9 +47,41 @@ console.log(upload, "up");
                   <li> <b>Email :</b>{details.email}</li> 
                  </div>
                  
+                </div> : <div className='details-container2' >
+                 <h1 className='heading'>User Details Of {details.name}</h1>
+                 <button onClick={handleEditClick}>Edit</button>
+                 <button onClick={handleEditClick}>Save</button>
+                 <div className='details'>
+                 <ChildComponent form = {form} formFunc = {setForm} 
+           error = {error}
+           upload = {upload}
+           uploadFunc = {setUpload}
+           errorFunc = {setError}
+           printFunc = {setPrint}
+           store = {store}
+           storeFunc = {setStore}
+           />
+                 </div>
                 </div>
             })}
-            </div>} 
+            </div>}  */}
+
+            {
+                 print && <div className='details-container'>
+                 {store.map((details)=>{
+                     return <div className='details-container2' >
+                      <h1 className='heading'>User Details Of {details.name}</h1>
+                     
+                      <div className='details'>
+                      <li> <b>Name :</b> {details.name}</li> 
+                       <li> <b>Age : </b>{details.age}</li>
+                       <li> <b>Gender :</b>{details.gender}</li>   
+                       <li> <b>Mobile :</b>{details.mobile}</li>  
+                       <li> <b>Email :</b>{details.email}</li> 
+                      </div>
+                     </div> 
+           })}
+           </div>}
         </>
     )
 }
